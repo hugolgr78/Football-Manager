b81c89b6d610074bd9974f0cd0ff884a76195f1c
Hugo Legrand <hugo.legrand1206@gmail.com>
2025-03-19 10:47:15 +0000

    fixed manager creatio

---
+++ tabs/refereeProfile.py
@@ -0,0 +1,95 @@
+import customtkinter as ctk
+from settings import *
+from data.database import *
+from data.gamesDatabase import *
+from PIL import Image
+import io
+
+class RefereeProfile(ctk.CTkFrame):
+    def __init__(self, parent, referee, changeBackFunction = None):
+        super().__init__(parent, fg_color = TKINTER_BACKGROUND, width = 1000, height = 700, corner_radius = 0)
+
+        self.parent = parent
+        self.referee = referee
+        self.changeBackFunction = changeBackFunction
+
+        self.profile = Profile(self, self.player)
+        self.titles = ["Profile"]
+        self.tabs = [self.profile]
+        self.classNames = [Profile]
+
+        self.activeButton = 0
+        self.buttons = []
+
+        self.tabsFrame = ctk.CTkFrame(self, fg_color = TKINTER_BACKGROUND, width = 1000, height = 50, corner_radius = 0)
+        self.tabsFrame.pack(expand = True, fill = "both", padx = 10, pady = 10)
+
+        self.createTabs()
+        self.profile.pack(expand = True, fill = "both")
+
+    def createTabs(self):
+
+        self.buttonHeight = 40
+        self.buttonWidth = 200
+        self.button_background = TKINTER_BACKGROUND
+        self.hover_background = GREY_BACKGROUND
+        self.gap = 0.102
+
+        gapCount = 0
+        for i in range(len(self.tabs)):
+            button = ctk.CTkButton(self.tabsFrame, text = self.titles[i], font = (APP_FONT, 20), fg_color = self.button_background, corner_radius = 0, height = self.buttonHeight, width = self.buttonWidth, hover_color = self.hover_background)
+            button.place(relx = self.gap * gapCount, rely = 0, anchor = "nw")
+            button.configure(command = lambda i = i: self.changeTab(i))
+            
+            gapCount += 2
+            self.buttons.append(button)
+            self.canvas(6, 55, self.gap * gapCount - 0.005)
+
+        self.buttons[self.activeButton].configure(state = "disabled")
+
+        ctk.CTkCanvas(self.tabsFrame, width = 1220, height = 5, bg = APP_BLUE, bd = 0, highlightthickness = 0).place(relx = 0, rely = 0.82, anchor = "w")
+
+        backButton = ctk.CTkButton(self.tabsFrame, text = "Back", font = (APP_FONT, 20), fg_color = TKINTER_BACKGROUND, corner_radius = 5, height = self.buttonHeight - 10, width = 100, hover_color = CLOSE_RED, command = lambda: self.changeBackFunction())
+        backButton.place(relx = 0.975, rely = 0, anchor = "ne")
+
+    def canvas(self, width, height, relx):
+        canvas = ctk.CTkCanvas(self.tabsFrame, width = width, height = height, bg = GREY_BACKGROUND, bd = 0, highlightthickness = 0)
+        canvas.place(relx = relx, rely = 0, anchor = "nw")
+
+    def changeTab(self, index):
+        self.buttons[self.activeButton].configure(state = "normal")
+        self.tabs[self.activeButton].pack_forget()
+        
+        self.activeButton = index
+        self.buttons[self.activeButton].configure(state = "disabled")
+
+        if not self.tabs[self.activeButton]:
+            self.tabs[self.activeButton] = globals()[self.classNames[self.activeButton].__name__](self, self.player)
+
+        self.tabs[self.activeButton].pack()
+
+class Profile(ctk.CTkFrame):
+    def __init__(self, parent, referee):
+        super().__init__(parent, fg_color = TKINTER_BACKGROUND, width = 1000, height = 630, corner_radius = 0) 
+
+        self.parent = parent
+        self.referee = referee
+
+        src = Image.open("Images/default_user.png")
+        src.thumbnail((200, 200))
+        self.photo = ctk.CTkImage(src, None, (src.width, src.height))
+        ctk.CTkLabel(self, image = self.photo, text = "").place(relx = 0.05, rely = 0.05, anchor = "nw")
+
+        ctk.CTkLabel(self, text = f"{self.referee.first_name} {self.referee.last_name}", font = (APP_FONT_BOLD, 40), fg_color = TKINTER_BACKGROUND).place(relx = 0.3, rely = 0.12, anchor = "w")
+
+        flag = Image.open(io.BytesIO(self.referee.flag))
+        flag.thumbnail((50, 50))
+        self.flag = ctk.CTkImage(flag, None, (flag.width, flag.height))
+        ctk.CTkLabel(self, image = self.flag, text = "").place(relx = 0.3, rely = 0.2, anchor = "w")
+        ctk.CTkLabel(self, text = self.referee.nationality.capitalize(), font = (APP_FONT, 20), fg_color = TKINTER_BACKGROUND).place(relx = 0.36, rely = 0.2, anchor = "w")
+
+        ctk.CTkLabel(self, text = f"{self.referee.age} years old / {self.referee.date_of_birth}", font = (APP_FONT, 20), fg_color = TKINTER_BACKGROUND).place(relx = 0.3, rely = 0.27, anchor = "w")
+        ctk.CTkLabel(self, text = "Referee", font = (APP_FONT, 20), fg_color = TKINTER_BACKGROUND).place(relx = 0.3, rely = 0.33, anchor = "w")
+
+        canvas = ctk.CTkCanvas(self, width = 1000, height = 5, bg = GREY_BACKGROUND, bd = 0, highlightthickness = 0)
+        canvas.place(relx = 0.5, rely = 0.4, anchor = "center")
